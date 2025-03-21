7cd62c25b1b928f549cbae07da956275900c153c
mathkimchi <kingalvinjkim@gmail.com>
2025-03-21 02:06:32 -0400

    building selector

--- Entities/Main Character/character_body_2d.gd
+++ Entities/Main Character/character_body_2d.gd
@@ -1,5 +1,15 @@
 extends CharacterBody2D
 
+# used for imports
+class_name MainCharacter
+
+enum Buildings { NONE, FARM, TEMPLE }
+
+# if index is -1, then nothing is selected
+@export
+var selected_building_index: int = -1
+#var selected_building: Buildings = Buildings.NONE
+
 
 const SPEED = 1000.0
 
@@ -9,3 +19,15 @@ func _physics_process(delta: float) -> void:
 	var direction = Input.get_vector("camera_left","camera_right","camera_up","camera_down")
 	velocity = (direction * SPEED )/ mainCam.zoom 
 	move_and_slide()
+
+func building_selector_clicked(index: int) -> void:
+	if selected_building_index==index:
+		# deselect
+		selected_building_index = -1
+	else:
+		selected_building_index = index
+	
+	# Update the is_selected for all the building buttons
+	# redundant but safer
+	for i in range(%Hotbar.get_child_count()):
+		%Hotbar.get_child(i).is_selected = i==selected_building_index
---
+++ ui/building_selector.gd
@@ -0,0 +1,38 @@
+extends Button
+
+@export
+var building_type: MainCharacter.Buildings = MainCharacter.Buildings.NONE
+var building_name: String
+
+var is_selected: bool = false
+
+## this is shared for all since static
+## accessed by the player
+## when selected is -1, then none is selected. But if 0 then first slot, if 1 then the next, ...
+#static var selected: int = -1
+
+# annoyingly, the main player script is inside the Character's rigidbody.
+@onready
+var player = get_node("/root/World/MainCharacter/CharacterBody2D")  
+
+func _ready() -> void:
+	match building_type:
+		MainCharacter.Buildings.FARM:
+			building_name = "Farm"
+		MainCharacter.Buildings.TEMPLE:
+			building_name = "Temple"
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	# Kinda wasteful, whatever
+	var display_string = "Build "
+	display_string += building_name
+	if is_selected:
+		display_string += "\n(Selected)"
+	
+	self.text = display_string
+
+
+func _pressed() -> void:
+	player.building_selector_clicked(get_parent().get_children().find(self))
