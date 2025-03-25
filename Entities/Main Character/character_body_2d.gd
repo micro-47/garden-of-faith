63428b2ac4a0293418576a6c4b7fffc4620b7aed
MrPoWasTaken <jerrygw2004@gmail.com>
2025-03-25 11:06:16 -0400

    added zoom limit and chunk loading

--- Entities/Main Character/character_body_2d.gd
+++ Entities/Main Character/character_body_2d.gd
@@ -4,8 +4,15 @@ extends CharacterBody2D
 const SPEED = 1000.0
 
 @onready var mainCam = $Camera
+@onready var generator = get_node("/root/TestWorld/NoiseGenerator")
 
 func _physics_process(delta: float) -> void:
 	var direction = Input.get_vector("camera_left","camera_right","camera_up","camera_down")
 	velocity = (direction * SPEED )/ mainCam.zoom 
 	move_and_slide()
+
+func _input (ev):
+	if Input.is_key_pressed(KEY_K):
+		print(generator)
+		generator.generate_chunk(Vector2(17,2))
+		print("works")
